e4f678455ecc070811c15b1f1ce8f86036e66b58
Maya Chisem <chisem.maya@gmail.com>
2020-07-17 13:49:13 -0700

    update

--- Devlop/js/script.js
+++ Devlop/js/script.js
@@ -1,2 +1,12 @@
+// variable
+var icon = document.querySelector("#icon");
+var links = document.getElementById("links");
 
+icon.addEventListener('click', function(){
+    if (links.style.display === "block") {
+      links.style.display = "none";
+    } else {
+      links.style.display = "block";
+    }
+});
 
