cac4bdf718a9032681407938056b4c22bcef65fe
Maya Chisem <chisem.maya@gmail.com>
2020-07-17 19:15:19 -0700

    update

--- Devlop/js/script.js
+++ Devlop/js/script.js
@@ -1,12 +1,12 @@
 // variable
-var icon = document.querySelector("#icon");
-var links = document.getElementById("links");
+// var icon = document.querySelector("#icon");
+// var links = document.getElementById("links");
 
-icon.addEventListener('click', function(){
-    if (links.style.display === "block") {
-      links.style.display = "none";
-    } else {
-      links.style.display = "block";
-    }
-});
+// icon.addEventListener('click', function(){
+//     if (links.style.display === "block") {
+//       links.style.display = "none";
+//     } else {
+//       links.style.display = "block";
+//     }
+// });
 
